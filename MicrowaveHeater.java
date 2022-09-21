004358d61b850fe40bb8366dc8f86fd5ca05801a
Jackson Naufal <Jackson.Naufal@mths.ca>
2022-09-21 00:27:51 +0000

    finishedAlmost

--- MicrowaveHeater.java
+++ MicrowaveHeater.java
@@ -17,17 +17,55 @@ import java.util.Scanner;
 
 final class MicrowaveHeater {
 
+    /**
+     * This is the subs base time.
+     */
     public static final double SUBTIME = 60;
+    /**
+     * This is the pizzas base time.
+     */
     public static final double PIZZATIME = 45;
+    /**
+     * This is the soups base time.
+     */
     public static final double SOUPTIME = 105;
+    /**
+     * This is the multiplier for two items.
+     */
     public static final double TWOFOOD = 1.5;
+    /**
+     * This is the multiplier for three items.
+     */
     public static final double THREEFOOD = 2;
+    /**
+     * This is the variable for a sub.
+     */
     public static final String SUB = "sub";
+    /**
+     * This is the variable for pizza.
+     */
     public static final String PIZZA = "pizza";
+    /**
+     * This is the variable for soup.
+     */
     public static final String SOUP = "soup";
+    /**
+     * This is the variable for one.
+     */
     public static final String ONE = "one";
+    /**
+     * This is the variable for two.
+     */
     public static final String TWO = "two";
+    /**
+     * This is the variable for three.
+     */
     public static final String THREE = "three";
+    /**
+     * This is the variable for 60 seconds.
+     */
+    public static final double SIXTY = 60;
+
     /**
     * Prevent instantiation.
     * Throw an exception IllegalStateException.
@@ -54,39 +92,38 @@ final class MicrowaveHeater {
         double seconds = 0;
 
         // max number in the Math.Random generator.
-        final Scanner FirstInput = new Scanner(System.in);
-        final Scanner NumberInput = new Scanner(System.in);
+        final Scanner firstInput = new Scanner(System.in);
+        final Scanner numberInput = new Scanner(System.in);
         System.out.print("Enter your choice, sub, pizza, or soup: ");
-        final String userInput = FirstInput.nextLine();
+        final String userInput = firstInput.nextLine();
         System.out.print("Enter the amount: ");
-        final String amountNumber = NumberInput.nextLine();
-        if (userInput.equals(SUB) || userInput.equals(PIZZA) || userInput.equals(SOUP)) {
+        final String amountNumber = numberInput.nextLine();
+        if (userInput.equals(SUB) || userInput.equals(PIZZA)
+            || userInput.equals(SOUP)) {
 
-        // try and catch for invalid inputs.
+            // try and catch for invalid inputs.
             if (userInput.equals(SUB)) {
                 time = SUBTIME;
-            }
-            else if (userInput.equals(PIZZA)) {
+            } else if (userInput.equals(PIZZA)) {
                 time = PIZZATIME;
-            }
-            else if (userInput.equals(SOUP)) {
+            } else if (userInput.equals(SOUP)) {
                 time = SOUPTIME;
             }
-        
             if (amountNumber.equals(ONE)) {
                 System.out.println("The time for " + userInput + " is" + time);
             } else if (amountNumber.equals(TWO)) {
-                time = (time * TWOFOOD);
+                time = time * TWOFOOD;
             } else if (amountNumber.equals(THREE)) {
-                 time = (time * THREEFOOD);
+                time = time * THREEFOOD;
             } else {
                 System.out.println("Invalid Input!");
             }
 
-            seconds = time / 60;
+            seconds = time / SIXTY;
             minutes = Math.floor(seconds);
-            seconds = (seconds - minutes) * 60;
-            System.out.println("The " + userInput + " will be done in " + minutes + " minutes " + seconds + " seconds!");
+            seconds = (seconds - minutes) * SIXTY;
+            System.out.println("The " + userInput + " will be done in "
+                + minutes + " minutes " + seconds + " seconds!");
 
         } else {
             System.out.println("Invalid Input");
